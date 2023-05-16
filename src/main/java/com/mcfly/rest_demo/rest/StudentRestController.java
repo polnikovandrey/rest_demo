66b19b15d798d6a4009ab3096a7f5a0057e930c7
polnikov.andrey <polnikov.andrey1981@gmail.com>
2023-05-16 18:45:00 +0300

    + /api/student/{studentId} endpoint (@PathVariable)

--- src/main/java/com/mcfly/rest_demo/rest/StudentRestController.java
+++ src/main/java/com/mcfly/rest_demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.mcfly.rest_demo.rest;
 
 import com.mcfly.rest_demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +14,23 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        final List<Student> students = new ArrayList<>();
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData() {
+        this.students = new ArrayList<>();
         students.add(new Student("Poornima", "Patel"));
         students.add(new Student("Mario", "Rossi"));
         students.add(new Student("Mary", "Smith"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
+    }
+
+    @GetMapping("/student/{studentId}")
+    public Student getStudentById(@PathVariable int studentId) {
+        return students.get(studentId);
     }
 }
